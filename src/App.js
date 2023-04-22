efe9ab92d9e92203c115b3604c39321c16057439
Anushka Verma <anushkaverma779@gmail.com>
2023-04-22 23:18:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Profile from "./Profile";
+import "./app.css";
+
+const tokenIdArr = [];
 
 function App() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLogin() {
+    if(username && password) {
+        fetch('https://dummyjson.com/auth/login', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+      
+          username: username,
+          password: password,
+        })
+        })
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+          let obj = {
+            id : data.id,
+            token : data.token
+          }
+          tokenIdArr.push(obj);
+          localStorage.setItem("tokenId", JSON.stringify(tokenIdArr));
+        })
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-container">
+        <h2>Login Page</h2>
+        <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} value={username} />
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
+        <button type="submit" onClick={handleLogin}>Login</button>
+      </div>
     </div>
   );
 }
